6c1e8ac1b9b271c016a10571a7a0bb0c23918836
Aidan Schrier <schrier.a@northeastern.edu>
2024-12-05 13:25:35 -0500

    Lots of assorted fixes

--- ui.py
+++ ui.py
@@ -72,9 +72,10 @@ class Python_Ui:
         choices = [game[1] for game in games]
         choices.append("None of the Above")
         choice = self.get_user_choice(choices)
-        if choice < len(games):
+        if choices[choice-1] != "None of the Above":
             cur.close()
-            return games[choice-1][0]
+            id = games[choice-1][0]
+            return id
             #Return the ID
         else:
             games = self.bgg.search_for_games(title)
@@ -151,6 +152,36 @@ class Python_Ui:
         cur.callproc('add_game_to_library', (library_id, game))
         cur.close()
         self.cnx.commit()
+    
+    def query_games(self):
+        print("Would you like to find a game to play in a friend's library, or find a brand new game to try?")
+        choice = self.get_user_choice(["Check a friend's library", "Query all known games"])
+        cur = self.cnx.cursor()
+        if choice == 1:
+            #Case that we are examining a friend's library
+            print("Whose library would you like to query?")
+            cur.callproc('get_friends_of', ('zimbo',))
+            friends = cur.fetchall()
+            friends = [tuple[0] for tuple in friends]
+            friends.append("I changed my mind")
+            friend = self.get_user_choice(friends)
+            if friend == len(friends):
+                choice = 2
+                #switch to other case
+            else:
+                friend = friends[friend-1]
+                print("Which library would you like to query?")
+                cur.callproc("get_libraries_for", (friend,))
+                libraries_tuple = cur.fetchall()
+                libraries = [tuple[1] for tuple in libraries_tuple]
+                libraries.append("None of these options")
+                library = libraries_tuple[self.get_user_choice(libraries)-1][0] #Accessing library ID
+                cur.callproc("recommend_games_from_library", ("zimbo", library))
+
+        if choice == 2:
+            #Case that we are querying ALL libraries
+            cur.callproc("recommend_games", ("zimbo",))
+        #NO PRINT EXISTS YET. DECIDE HOW WE WANT TO DISPLAY THIS
         
 
     def run_main_loop(self):
@@ -175,7 +206,7 @@ class Python_Ui:
                 case 3:
                     self.rate_game()
                 case 4:
-                    print("FINDING GAME")
+                    self.query_games()
                 case 5:
                     print("DELETING ACCOUNT")
                     running = False
